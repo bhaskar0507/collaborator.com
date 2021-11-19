9bec2e8d3619ebf9fcec3c8856e210a479ad0c79
Bhaskar Rai <bhaskar9893@gmail.com>
2021-11-20 05:21:46 +0530

    profile page completed

--- Frontend/collaborator/src/App.js
+++ Frontend/collaborator/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-import DetailPage from "./components/Detail_Page/DetailPage";
+// import DetailPage from "./components/Detail_Page/DetailPage";
+import Profile from "./components/Profile_Page/Profile";
 
 function App() {
   return (
     <div className="App">
-     <h1>This is collaborator .com</h1>
-     <DetailPage/>
+     {/* <h1>This is collaborator .com</h1> */}
+     {/* <DetailPage/> */}
+     <Profile />
     </div>
   );
 }
