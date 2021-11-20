8720b1250749f3c64e65ce783bf8874433751b20
Bhaskar Rai <bhaskar9893@gmail.com>
2021-11-20 16:41:09 +0530

    login signip page added

--- Frontend/collaborator/src/App.js
+++ Frontend/collaborator/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import DetailPage from "./components/Detail_Page/DetailPage";
 import Profile from "./components/Profile_Page/Profile";
 import FormPropsTextFields from "./components/Signup_Page/Signup"
-import Login from "./components/Login_Page/Login"
+import Login from "./components/Login_Page/Login";
+
 
 function App() {
   return (
